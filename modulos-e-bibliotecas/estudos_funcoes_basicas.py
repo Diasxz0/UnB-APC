ca60632393569501284f94c87bfb5d513805d4d4
Warlley Dias <wmedeiros.d@gmail.com>
2026-04-10 11:36:25 -0300

    Add files via upload

---
+++ modulos-e-bibliotecas/estudos_funcoes_basicas.py
@@ -0,0 +1,25 @@
+# def soma (n):
+#     x1 = float(input("Qual o primeiro numero?"))
+#     x2 = float(input("Qual o segundo numero?"))
+#     s = x1 + x2
+#     return s
+# s2 = soma(float())
+# print(s2)
+
+# def dobrar(n):
+#     d = 2 * n
+#     return d
+
+# r = dobrar(float(input("Qual o numero que deseja dobrar?")))
+# print(r)
+
+# def media (n1, n2):
+#    return (n1 + n2) / 2
+
+# n1 = 10
+# n2 = 20
+
+# print(media(n1,n2))
+
+
+
